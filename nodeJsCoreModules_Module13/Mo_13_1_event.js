b595dcb7261c5b59a7cf6d45e75ccafbe11eccdc
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-05-30 21:53:52 +0600

    update

---
+++ nodeJsCoreModules_Module13/Mo_13_1_event.js
@@ -0,0 +1,56 @@
+
+const EventEmitter = require('node:events');
+
+class SchoolBell extends EventEmitter {}
+
+const schoolBell = new SchoolBell();
+
+schoolBell.on('ring', ()=>{ // ring is event name
+    console.log("Yahoo!! Class sesh"); //event listener (মানে, event টা ঘটলে কী হবে সেটা বলে দেওয়া)
+})
+schoolBell.on('ring', ()=>{
+    console.log("Oh No!! R o ekta class ache");
+})
+schoolBell.on('broken', ()=>{
+    console.log("class ki sesh hobe na?");
+})
+
+schoolBell.emit('ring'); //event trigger (emit) করছো — মানে bell বাজানোর মতো।
+schoolBell.emit('broken');
+
+
+/*
+Node.js-এ Event কী?
+
+Event মানে হলো "ঘটনা" — যেমন তোমার মোবাইলে notification আসা। Node.js এ Event system ব্যবহার করা হয় asynchronous ভাবে কাজ চালানোর জন্য। এটা অনেকটা real-life school bell-এর মতো:
+
+Bell বাজে (event emit হয়)
+Student বা teacher রা react করে (event listener respond করে)
+Node.js-এ built-in events module দিয়ে আমরা event emit (ঘটানো) আর listen (শোনা) করতে পারি।
+
+
+const EventEmitter = require('node:events');
+🔹 Node.js থেকে events module import করা হচ্ছে।
+🔹 এর ভিতরে আছে EventEmitter নামের class, যেটা দিয়ে event তৈরি ও handle করা যায়।
+
+class SchoolBell extends EventEmitter {}
+🔹 তুমি SchoolBell নামে একটা class বানিয়েছো, যেটা EventEmitter কে extend করে।
+🔹 মানে SchoolBell এখন এমন একটা class যেটা নিজে event emit করতে পারে এবং event শুনতেও পারে।
+
+const schoolBell = new SchoolBell();
+🔹 SchoolBell এর একটা object বানানো হয়েছে — schoolBell।
+🔹 এখন এটায় তুমি event attach করতে পারবে এবং emit করতে পারবে।
+
+
+Method--------------------------কাজ
+
+on() --------------------বারবার event handle করে
+once()---------------------------	শুধু একবার চলে
+emit()---------------------------	event trigger করে
+removeListener() / off()---------------------------	listener সরিয়ে দেয়
+listenerCount()---------------------------	listener সংখ্যা দেখে
+setMaxListeners(n)---------------------------	warning না দিয়ে n টা পর্যন্ত listener allow করে
+eventNames()---------------------------	active event list দেয়
+prependListener()---------------------------	listener কে সবার আগে রাখে
+
+*/
---
+++ nodeJsCoreModules_Module13/Mo_13_2_3_fileSystem.js
@@ -0,0 +1,71 @@
+// synchronous way
+// file read or I/O intensive task ----> single thread(complete task) --> not go thread pool.
+
+//asynchronous way
+// file read ----> single thread -> event loop -> thread pool(File Read/Write শেষ হলে Callback Queue তে পাঠায়) -> task completed (Main thread callback execute করে).
+
+//synchronous
+
+/*
+
+const fs = require('fs');
+
+console.log('Task 1');
+
+const text = "learning file system";
+fs.writeFileSync('./hello.txt', text); //task 2
+
+console.log('task 3');
+
+const data = fs.readFileSync('./hello.txt', {encoding: 'utf-8'});
+console.log(data); //task 4
+
+console.log('task 5');
+
+*/
+
+//asynchronous
+
+const fs = require('fs');
+
+console.log('Task 1');
+
+let text = 'node js';
+
+fs.writeFile('./hello.txt', text, {encoding: 'utf-8'}, (err) =>{ //asynchronous a all time ekta call back function thakbe.
+    if(err){
+        console.log('Something went wrong', err);
+        return;
+    }
+    console.log('Written successful') // eta asynchronous taai sobar last a print korbe.
+})
+
+fs.readFile('./hello.txt', {encoding: 'utf-8'}, (err, data) =>{ //asynchronous a all time ekta call back function thakbe.
+    if(err){
+        console.log('Something went wrong', err);
+        return;
+    }
+    text = data;
+
+    console.log(text, "inside callback"); // eta asynchronous taai sobar last a print korbe.
+});
+
+console.log(text, "from console");
+
+console.log('Task 3');
+
+
+
+/*
+
+Node.js এ fs module ব্যবহার করে তুমি file read, write, update, delete ইত্যাদি করতে পারো। এটা Node.js-এর built-in module — আলাদা install করতে হয় না।
+
+কাজের ধরন দুইভাবে হয়:
+
+Asynchronous (Non-blocking) — fast, callback/Promise based
+Synchronous (Blocking) — সরাসরি কাজ করে, কিন্তু পুরো প্রোগ্রামকে আটকে রাখে
+
+যেহেতু writeFileSync() এবং readFileSync() blocking, তাই পরবর্তী লাইন এক্সিকিউট হতে অপেক্ষা করে। পুরো task একটার পর একটা চলে।
+
+Thread pool-------------  Node.js এর পিছনে থাকা worker threads যা async I/O task করে
+*/
